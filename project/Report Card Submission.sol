209e819bab0831d3f51933c0a7bc9eb645be25e4
Salmankhan02 <47716827+Salmankhan02@users.noreply.github.com>
2020-07-29 15:35:19 +0530

    Add files via upload

---
+++ project/Report Card Submission.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.21 < 0.6.12;
+contract report{
+    string name;
+    int rollno;
+    string batch;
+    int mark1;
+    int mark2;
+    int mark3;
+    int mark4;
+    string status;
+    function report(string newname,
+    int newrollno,
+    string newbatch,
+    int newmark1,
+    int newmark2,
+    int newmark3,
+    int newmark4,
+    string newstatus)public{
+    name=newname;
+    rollno=newrollno;
+    batch=newbatch;
+    mark1=newmark1;
+    mark2=newmark2;
+    mark3=newmark3;
+    mark4=newmark4;
+    status=newstatus;
+        
+    }
+    function showReport()public view returns(string,
+    int,
+    string,
+    int,
+    int,
+    int,
+    int,
+    string){
+        return(name,rollno,batch,mark1,mark2,mark3,mark4,status);
+    }
+}
